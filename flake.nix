3c4f7b76a7c969d4abf0d0b717111911324b95a2
Rasmus Enevoldsen <rasmus@enev.dk>
2024-11-01 17:40:41 +0100

    init: setup dev env for embedded rust
    This is the base commit.
    This dev env is nix based and uses
     - flakes
     - direnv
     - projectile for emacs

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      overlays = [
+        rust-overlay.overlays.default
+        (final: prev: {
+          rustToolchain = let rust = prev.rust-bin;
+          in if builtins.pathExists ./rust-toolchain.toml then
+            rust.fromRustupToolchainFile ./rust-toolchain.toml
+          else if builtins.pathExists ./rust-toolchain then
+            rust.fromRustupToolchainFile ./rust-toolchain
+          else
+            rust.stable.latest.default.override {
+              extensions = [ "rust-src" "rustfmt" ];
+            };
+        })
+      ];
+      thumbv7emPkgs = import nixpkgs {
+        system = "x86_64-linux";
+        crossSystem = nixpkgs.lib.systems.examples.arm-embedded // {
+          rustc.config = "thumbv7em-none-eabi";
+        };
+        config.allowUnsupportedSystem = true;
+      };
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit overlays system; }; });
+    in {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              (rust-bin.stable.latest.default.override {
+                extensions = [ "rust-src" ];
+                targets = [ "thumbv7em-none-eabihf" ];
+              })
+            ] ++ [
+              rustToolchain
+              openssl
+              pkg-config
+              cargo
+              cargo-deny
+              cargo-edit
+              cargo-watch
+              rust-analyzer
+              # Embedded
+              probe-run
+              flip-link
+              probe-rs
+              openocd
+            ];
+        };
+      });
+    };
+}
